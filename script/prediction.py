1af6fdffbdccef94692da0183c255331511c71f5
guruttosekai2011 <guruttosekai2011@gmail.com>
2015-09-07 21:18:37 +0900

    make prediction.py #7

--- script/prediction.py
+++ script/prediction.py
@@ -8,8 +8,16 @@ import sklearn.linear_model
 import sklearn.ensemble
 import img_to_pickle as i_p
 import features as f
+import classify
+import preprocessing as pre
 import numpy as np
 import pandas as pd
+import datetime
+import os
+
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+SUBMISSION_DIR = ROOT.replace("script", "tmp/submission")
 
 
 clf_dict = {
@@ -22,14 +30,26 @@ clf_dict = {
         "clf": sklearn.ensemble.GradientBoostingClassifier(random_state=1, learning_rate=0.005, n_estimators=50, \
                 subsample=1.0, max_features=1.0, min_samples_split=2, min_samples_leaf=2, max_depth=2)
     },
+    'SR':{   
+        "name": 'SGD Regression',
+        "clf": sklearn.linear_model.SGDRegressor(penalty='l2'),
+    }
 }
+
+
+def zero_one(x):
+    
+    return min(max(x, 0.),1.)
 
 
 def convert_testdata(test_gray_data):
 
     data_df = f.make_test_df(test_gray_data) 
     fu = FeatureUnion(transformer_list=f.feature_transformer_rule)
+    Std = preprocessing.StandardScaler()
+    
     X_test = fu.fit_transform(data_df)
+    X_test = Std.fit_transform(X_test)
     
     return X_test
 
@@ -38,33 +58,91 @@ def convert_traindata(train_gray_data, labels):
 
     data_df = f.make_data_df(train_gray_data, labels) 
     fu = FeatureUnion(transformer_list=f.feature_transformer_rule)
+    Std = preprocessing.StandardScaler()
+    
     X_train = fu.fit_transform(data_df)
     y_train = np.concatenate(data_df["label"].apply(lambda x: x.flatten()))
+    
+    X_train = Std.fit_transform(X_train)
 
     return X_train, y_train
 
 
 def prediction(clf_name):
+    
+    
+    print SUBMISSION_DIR
+    print submission_path
+    quit()
 
     print "****************classifier****************"
     print clf_dict[clf_name]["clf"]
     clf = clf_dict[clf_name]["clf"]
-    
+   
     _, _, _, train_gray_data, test_gray_data, _, labels = i_p.load_data()
-    X_train, y_train = convert_traindata(train_gray_data, labels)
-    X_test = convert_testdata(test_gray_data)
+    train_keys = train_gray_data.keys() 
+    test_keys = test_gray_data.keys() 
+    
+    train_df = f.make_data_df(train_gray_data, labels)
+    test_df = f.make_test_df(test_gray_data)
 
-    print "... training"
-    clf.fit(X, y)
+    train_df = train_df.reset_index()
+    test_df = test_df.reset_index()
+    
+    train_df.columns = ["pngname", "input", "label"]
+    test_df.columns = ["pngname", "input"]
+   
+    #operation check
+    #train_df, train_keys, test_df, test_keys  = pre.make_checkdata(mode="df")
 
-    print "... test"
-    output = clf.predict(X_test)
+    
+    for i in xrange(len(train_keys)):
 
+        train_X, train_y = classify.set_traindata(train_df, train_keys[i])
+        clf.partial_fit(train_X, train_y) 
+    
+    
+    for i in xrange(len(test_keys)):
+
+        test_img = test_df[(test_df["pngname"] == test_keys[i])]["input"].as_matrix()[0]
+        
+        imgname = test_keys[i]
+        shape = test_img.shape
+
+        test_img = {test_keys[i]:test_img}
+        X_test = convert_testdata(test_img)
+        output = clf.predict(X_test)
+        output = np.asarray(output)
+        zo = np.vectorize(zero_one)
+        output = zo(output).reshape(shape)
+
+        tmp = []
+
+        for row in xrange(len(output)):
+            for column in xrange(len(output[row])):
+                id_ = imgname + "_" + str(row+1) + "_" + str(column+1)
+                value = output[row][column]
+
+                pix = [id_, value]
+                tmp.append(pix)
+
+        if i == 0:
+            predict_df = pd.DataFrame(tmp)
+
+        else:
+            tmp_df = pd.DataFrame(tmp)
+            predict_df = pd.concat([predict_df, tmp_df])
+
+    predict_df.columns = ["id", "value"] 
+
+    now = datetime.datetime.now()
+    submission_path = SUBMISSION_DIR + "/submission_" + now.strftime("%Y_%m_%d_%H_%M_%S") + ".csv"
+    predict_df.to_csv(submission_path, header=True, index=False)
 
 
 if __name__ == '__main__':
 
-    clf_name = "LR"
+    clf_name = "SR"
     prediction(clf_name)
 
 
